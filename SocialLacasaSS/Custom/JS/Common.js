c99560e385602984befa17e81cfa645d50cb3234
shaheenbohra <43819905+shaheenbohra@users.noreply.github.com>
2018-11-28 18:43:39 +0530

    -new

---
+++ SocialLacasaSS/Custom/JS/Common.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    var selector = '.nav li';
+
+    $(selector).on('click', function () {
+        $(selector).removeClass('active');
+        $(this).addClass('active');
+        
+    });
+    var isAdmin = localStorage["isAdmin"];
+   
+    if (isAdmin == "1") {
+        $('.clsUser').hide();
+        $('.clsadmin').show();
+    }
+    else {
+        $('.clsUser').show();
+        $('.clsadmin').hide();
+    }
+})
+
+var LogOut = function () {
+   
+
+  
+        
+        $.ajax({
+            type: "POST",
+            url: "/Service/logOut",
+          
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                localStorage["isAdmin"]=null;
+                    window.location.href = "/Visitor/SignIn";
+
+                
+            },
+            error: function (err) { }
+        });
+
+    
+}
+
